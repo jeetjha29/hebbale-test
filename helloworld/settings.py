fa73756cc219c3ab59b56076ea4b5a9573c3c328
jeetu@hebbale.academy <jeetu@hebbale.academy>
2023-04-21 03:19:33 +0000

    hello changes

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-dvi=hh&7^m%sevt6x+-j%9pdcalt03+77w4b5x6(mc$l_tts8(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-dev-kode-ws-81df1b4f4.hebbale.academy']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,10 +75,7 @@ WSGI_APPLICATION = "helloworld.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": {}
 }
 
 
